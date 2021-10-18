4753027833336776eca089dd2fbc520654ad8b93
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-18 12:59:14 -0400

    updating component names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React from "react";
-import Main from "./components/Navbar";
+import NavMain from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <Main />
+      <NavMain />
     </>
   );
 }
