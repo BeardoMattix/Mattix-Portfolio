71fae684b8d2b653e4ad54f6ddc1a5b59d8c2df2
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-21 12:25:04 -0400

    updating link to download resume

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,7 +13,8 @@ export default function About() {
             alt="..."
             className="img-thumbnail rounded-circle"
           ></img>
-          <p className="about-me-content">
+
+          <p className=" col about-me-content">
             {" "}
             Hello and thanks for checking out my portfolio! My name is Chris
             Mattix and I am currently a student in the UNC Full Stack Developer
@@ -24,10 +25,11 @@ export default function About() {
             <br></br>I am originally from Montana, and miss my home state
             everyday. I have lived in North Carolina for the past 6 years, and
             truly love it...even if the mountains here don't have the same
-            majesty as the ones in Montana.<br></br>I am an avid weigthlifter,
-            home cook, coffee afficianado, book nerd, and music lover. I am a
-            proud resident of Durham, NC and am excited to enter the world of
-            web development full-time once my coursework has concluded.
+            majesty as the ones in Montana.<br></br>
+            <br></br>I am an avid weigthlifter, home cook, coffee afficianado,
+            book nerd, and music lover. I am a proud resident of Durham, NC and
+            am excited to enter the world of web development full-time once my
+            coursework has concluded.
           </p>
         </div>
         <div class="col"></div>
